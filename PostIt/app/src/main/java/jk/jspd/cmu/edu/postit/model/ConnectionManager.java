74562400b05229670c7850da6c7315a79b7af42c
jason <lavalakewl@gmail.com>
2015-11-13 18:37:58 -0800

    pull

--- PostIt/app/src/main/java/jk/jspd/cmu/edu/postit/model/ConnectionManager.java
+++ PostIt/app/src/main/java/jk/jspd/cmu/edu/postit/model/ConnectionManager.java
@@ -1,10 +1,8 @@
 package jk.jspd.cmu.edu.postit.model;
 
 import org.json.JSONObject;
-
-import jk.jspd.cmu.edu.postit.ws.remote.ConnectionInterface;
 import jk.jspd.cmu.edu.postit.ws.remote.FacebookInterface;
-
+import jk.jspd.cmu.edu.postit.ws.remote.ConnectionInterface;
 /**
  * Created by lavalake on 11/13/15.
  */
